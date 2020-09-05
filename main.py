d75749ebbaf4ca6954fb1881d843cc8c1fe45b72
Sinaru Gunawardena <sinarug@gmail.com>
2020-09-05 15:57:24 +0100

    move function handlers to dict

--- main.py
+++ main.py
@@ -2,18 +2,17 @@
 
 import pydevd_pycharm
 import yaml
-import os
-
+import lib.manager as manager
 from lib.handlers import process_apt_packages, process_apt_keys, process_sources, process_file_block, \
     process_systemd_services
 
 # remote debugging
 pydevd_pycharm.settrace('localhost', port=8080, stdoutToServer=True, stderrToServer=True)
 
-if not os.path.exists('.automata/'):
-    os.mkdir('.automata')
+manager.init()
+args = manager.console_input_args()
 
-with open('examples/lab/lab_1.yml') as file:
+with open(args.file) as file:
     content = yaml.load(file)
 
 version = content.pop('version', '')
@@ -21,17 +20,19 @@ version = content.pop('version', '')
 if version != '0.1':
     raise Exception("Unknown version")
 
+key_function = {
+    'apt_packages': 'process_apt_packages',
+    'apt_keys': 'process_apt_keys',
+    'sources': 'process_sources',
+    'files': 'process_file_block',
+    'systemd.services': 'process_systemd_services'
+}
+
 for key in content.keys():
     data = content[key]
-    if key == 'apt_packages':
-        process_apt_packages(data)
-    elif key == 'apt_keys':
-        process_apt_keys(data)
-    elif key == 'sources':
-        process_sources(data)
-    elif key == 'files':
-        process_file_block(data)
-    elif key == 'systemd.services':
-        process_systemd_services(data)
-    else:
+    function = key_function.get(key, False)
+
+    if not function:
         raise Exception(f"Invalid type {key}")
+
+    locals()[function](data)
